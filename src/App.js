c7df71c3705433e13d4157f656497828984b650e
Julaam Diop <jeebe.diop@gmail.com>
2021-04-26 14:40:55 -0500

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,63 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { CardList } from './components/card-list/card-list.component'
+import { SearchBox } from './components/search-box/search-box.component'
 import './App.css';
 
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+
+    // context of this in handleChange is this keyword (binding handleChange to this.)
+    //this.handleChange = this.handleChange.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json()
+    .then(users => this.setState({monsters: users})));
+  }
+  // Function to handleChange in search bar
+  /*
+  handleChange(e) {
+    this.setState({searchField: e.target.value})
+  }
+*/
+// will automatically bind this keyword for handleChange
+  handleChange = (e) => {
+    this.setState({searchField: e.target.value})
+  }
+
+  render() {
+    // destructuring our array to search properly
+    // easier way of typing
+    // cont monsters = this.state.monsters
+    // const searchField = this.state.searchField
+    const { monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase() // makes sure search is not case senstiive
+      .includes(searchField.toLowerCase()) // includes search text and ensures not case sensitive
+
+    );
+
+    return ( // returning so it works
+      <div className="App">
+        <h1> Monsters Rolodex </h1>
+        <SearchBox
+          placeholder='Search Monsters'
+          handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters}>
+        </CardList>
+
+      </div>
+        );
+      }
+  }
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +78,5 @@ function App() {
     </div>
   );
 }
-
+*/
 export default App;
